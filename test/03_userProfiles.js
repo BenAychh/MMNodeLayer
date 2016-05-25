67ef0de1e76d6aa1a4add0a48b25435c47dd3504
David Sudia <dsudia@gmail.com>
2016-05-25 14:53:09 -0600

    wrote profile creation tests

--- test/03_userProfiles.js
+++ test/03_userProfiles.js
@@ -8,52 +8,265 @@ chai.use(chaiHttp);
 
 describe('can create a user\'s detailed profile', () => {
     it('should create a profile with all information', done => {
-
+        chai.request(server)
+            .post('/profiles/create')
+            .send({
+                email: 'testy@test.com',
+                displayName: 'Testy',
+                lastName: 'Mctestface',
+                description: 'Quis aute iure reprehenderit in voluptate velit esse. Mercedem aut nummos unde unde extricat, amaras. Morbi odio eros, volutpat ut pharetra vitae, lobortis sed nibh. Ab illo tempore, ab est sed immemorabili. Gallia est omnis divisa in partes tres, quarum.',
+                state: 'CO',
+                avatarUrl: 'http://s3.aws.com/someimage0908234.jpg'
+            })
+            .end((err, res) => {
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.status.should.equal(201);
+                res.body.message.should.equal('Profile created for testy@test.com');
+                done();
+            });
     })
     it('should create a profile with just an email and displayName', done => {
-
+        chai.request(server)
+            .post('/profiles/create')
+            .send({
+                email: 'testy@test.com',
+                displayName: 'Testy'
+            })
+            .end((err, res) => {
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.status.should.equal(201);
+                res.body.message.should.equal('Profile created for testy@test.com');
+                done();
+            })
     })
     it('should return an error if email is missing', done => {
-
+        chai.request(server)
+            .post('/profiles/create')
+            .send({
+                displayName: 'Testy'
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.should.be.json;
+                res.should.be.a('object');
+                res.body.status.should.equal(400);
+                res.body.message.should.equal('Missing email or display name for creation');
+                done();
+            })
     })
     it('should return an error if email is malformed', done => {
-
+        chai.request(server)
+            .post('/profiles/create')
+            .send({
+                email: 'testy@test',
+                displayName: 'Testy'
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.should.be.json;
+                res.should.be.a('object');
+                res.body.status.should.equal(400);
+                res.body.message.should.equal('Please provide a valid email');
+                done();
+            })
     })
     it('should return an error if displayName is missing', done => {
-
+        chai.request(server)
+            .post('/profiles/create')
+            .send({
+                email: 'testy@test.com'
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.should.be.json;
+                res.should.be.a('object');
+                res.body.status.should.equal(400);
+                res.body.message.should.equal('Missing email or display name for creation');
+                done();
+            })
     })
     it('should return an error if displayName isn\'t a string', done => {
-
+        chai.request(server)
+            .post('/profiles/create')
+            .send({
+                email: 'testy@test.com',
+                displayName: 1000
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.should.be.json;
+                res.should.be.a('object');
+                res.body.status.should.equal(400);
+                res.body.message.should.equal('Missing email or display name for creation');
+                done();
+            })
     })
     it('should return an error if displayName is greater than 50 characters', done => {
-
+        chai.request(server)
+            .post('/profiles/create')
+            .send({
+                email: 'testy@test.com',
+                displayName: 'Ab illo tempore, ab est sed immemorabili. Nihilne te nocturnum praesidium Palati, nihil urbis vigiliae. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Qui ipsorum lingua Celtae, nostra Galli appellantur.'
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.should.be.json;
+                res.should.be.a('object');
+                res.body.status.should.equal(400);
+                res.body.message.should.equal('Missing email or display name for creation');
+                done();
+            })
     })
     it('should return an error if the user already exists', done => {
-
+        chai.request(server)
+            .post('/profiles/create')
+            .send({
+                email: 'testy@test.com',
+                displayName: 'Testy'
+            })
+            .end((err, res) => {
+                if (!err) {
+                    chai.request(server)
+                        .post('/profiles/create')
+                        .send({
+                            email: 'testy@test.com',
+                            displayName: 'Testy',
+                        })
+                        .end((err, res) => {
+                            res.should.have.status(409);
+                            res.should.be.json;
+                            res.body.should.be.a('object');
+                            res.body.status.should.equal(409);
+                            res.body.message.should.equal('Profile already exists');
+                            res.body.email.should.equal('testy@test.com');
+                            res.body.displayName.should.equal('Testy');
+                            done();
+                        })
+                }
+            });
     })
     it('should return an error if the lastName exists and isn\t a string', done => {
-
+        chai.request(server)
+            .post('/profiles/create')
+            .send({
+                email: 'testy@test.com',
+                displayName: 'Testy',
+                lastName: 1000
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.status.should.equal(400);
+                res.body.message.should.equal('Please enter a last name that is text and less than 30 characters');
+                done();
+            });
     })
     it('should return an error if the lastName exists and is longer than 30 characters', done => {
-
+        chai.request(server)
+            .post('/profiles/create')
+            .send({
+                email: 'testy@test.com',
+                displayName: 'Testy',
+                lastName: 'Quis aute iure reprehenderit in voluptate velit esse. Mercedem aut nummos unde unde extricat, amaras. Morbi odio eros, volutpat ut pharetra vitae, lobortis sed nibh. Ab illo tempore, ab est sed immemorabili. Gallia est omnis divisa in'
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.status.should.equal(400);
+                res.body.message.should.equal('Please enter a last name that is text and less than 30 characters');
+                done();
+            });
     })
     it('should return an error if the description exists and isn\'t a string', done => {
-
+        chai.request(server)
+            .post('/profiles/create')
+            .send({
+                email: 'testy@test.com',
+                displayName: 'Testy',
+                description: 1000
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.status.should.equal(400);
+                res.body.message.should.equal('Please enter a description that is text and less than 500 characters');
+                done();
+            });
     })
     it('should return an error if the description exists and is longer than 500 characters', done => {
-
+        chai.request(server)
+            .post('/profiles/create')
+            .send({
+                email: 'testy@test.com',
+                displayName: 'Testy',
+                description: 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus. Mercedem aut nummos unde unde extricat, amaras. Quam diu etiam furor iste tuus nos eludet? Prima luce, cum quibus mons aliud consensu ab eo. Quis aute iure reprehenderit in voluptate velit esse. Phasellus laoreet lorem vel dolor tempus vehicula. Ullamco laboris nisi ut aliquid ex ea commodi consequat. Salutantibus vitae elit libero, a pharetra augue. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Praeterea iter est quasdam res quas ex communi. Curabitur est gravida et libero vitae dictum. Contra legem facit qui id facit quod lex prohibet. Plura mihi bona sunt, inclinet, amari petere vellent. Quisque ut dolor gravida, placerat libero vel, euismod. Quid securi etiam tamquam eu fugiat nulla pariatur. A communi observantia non est recedendum. Non equidem invideo, miror magis posuere velit aliquet. Quae vero auctorem tractata ab fiducia dicuntur. Nec dubitamus multa iter quae et nos invenerat. Hi omnes lingua, institutis, legibus inter se differunt. Etiam habebis sem dicantur magna mollis euismod. Quam temere in vitiis, legem sancimus haerentia. Idque Caesaris facere voluntate liceret: sese habere. Ab illo tempore, ab est sed immemorabili. Paullum deliquit, ponderibus modulisque suis ratio utitur. Cras mattis iudicium purus sit amet fermentum.'
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.status.should.equal(400);
+                res.body.message.should.equal('Please enter a description that is text and less than 500 characters');
+                done();
+            });
     })
     it('should return an error if state exists and is not a string', done => {
-
+        chai.request(server)
+            .post('/profiles/create')
+            .send({
+                email: 'testy@test.com',
+                displayName: 'Testy',
+                state: 1000
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.status.should.equal(400);
+                res.body.message.should.equal('Please enter a two-letter state abbreviation');
+                done();
+            });
     })
     it('should return an error if state exists and is longer than two characters', done => {
-
+        chai.request(server)
+            .post('/profiles/create')
+            .send({
+                email: 'testy@test.com',
+                displayName: 'Testy',
+                state: 'Colorado'
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.status.should.equal(400);
+                res.body.message.should.equal('Please enter a two-letter state abbreviation');
+                done();
+            });
     })
-    it('should return an error if the avatar file exists and is not a png or jpg', done => {
-
-    })
-    it('should return an error if the avatar file exists and is greater than 1MB', done => {
-
+    it('should return an error if the avatarUrl is not a full (protocol, domain, URI) valid URL', done => {
+        chai.request(server)
+            .post('/profiles/create')
+            .send({
+                email: 'testy@test.com',
+                displayName: 'Testy',
+                avatarUrl: 'ww.funky.gov/alksdjf'
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.status.should.equal(400);
+                res.body.message.should.equal('Please enter a valid url for your profile image');
+                done();
+            });
     })
 });
 describe('get user profile', () => {
