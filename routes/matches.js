34f075d1c3a2e5487093d48ea5370f6f0a28757b
BenAychh <ben.tomas.hernandez@gmail.com>
2016-05-30 15:27:41 -0600

    Test 12 passing

--- routes/matches.js
+++ routes/matches.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var jwt = require('jsonwebtoken');
 var rp = require('request-promise');
 var matchService = 'http://localhost:8002/';
+var emailValidator = require('email-validator');
 
 var secretKey = process.env.secretKey;
 if (!secretKey) {
@@ -47,6 +48,73 @@ router.get('/suggested', (req, res, next) => {
         return;
       }
     });
+  } else {
+    res.status(401);
+    res.json({
+      message: 'Please log in',
+      status: 401,
+    })
+    return;
+  }
+})
+
+router.put('/interest', (req, res, next) => {
+  if (req.body.token) {
+    if (req.body.interestedIn) {
+      if (emailValidator.validate(req.body.interestedIn)) {
+        jwt.verify(req.body.token, secretKey, function(err, decoded) {
+          if (!err) {
+            let interestOptions = {
+              method: 'PUT',
+              uri: matchService + "addinterest",
+              body: {
+                  email: decoded.email,
+                  interestedIn: req.body.interestedIn,
+              },
+              json: true // Automatically stringifies the body to JSON
+            };
+            rp(interestOptions)
+            .then(parsedBody => {
+              console.log(parsedBody);
+              res.status(200);
+              res.json({
+                message: parsedBody.message,
+                status: 200,
+              })
+            })
+            .catch(errorBody => {
+              res.status(errorBody.statusCode);
+              res.json({
+                message: errorBody.message,
+                status: errorBody.statusCode,
+              });
+              return;
+            })
+          } else {
+            res.status(401);
+            res.json({
+              message: 'Invalid token, please log out then log back in',
+              status: 401,
+            })
+            return;
+          }
+        });
+      } else {
+        res.status(400);
+        res.json({
+          message: 'Invalid email, check the syntax and try again',
+          status: 400,
+        })
+        return;
+      }
+    } else {
+      res.status(400);
+      res.json({
+        status: 400,
+        message: 'Please include the email of the user you are interested in',
+      })
+      return;
+    }
   } else {
     res.status(401);
     res.json({
